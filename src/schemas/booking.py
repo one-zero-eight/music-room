a62dba528b93cc964d60370e908a2449bf24ac71
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-12-25 02:17:12 +0300

    chore: fix time in `/bookings/create_booking` route

--- src/schemas/booking.py
+++ src/schemas/booking.py
@@ -1,14 +1,16 @@
 import datetime
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, NaiveDatetime, Field
 
 __all__ = ["CreateBooking", "ViewBooking", "HelpBooking"]
+
+_now = datetime.datetime.now().replace(second=0, microsecond=0, tzinfo=None)
 
 
 class CreateBooking(BaseModel):
     participant_id: int
-    time_start: datetime.datetime
-    time_end: datetime.datetime
+    time_start: NaiveDatetime = Field(..., example=_now)
+    time_end: NaiveDatetime = Field(..., example=_now + datetime.timedelta(hours=1))
 
 
 class ViewBooking(BaseModel):
