08955e24c3240049555d17508bd3d360e75201bd
evermake <vladeryabkin@gmail.com>
2024-01-14 13:13:25 +0500

    fix: improve verification code e-mail template
    - new e-mail HTML template created with MJML
    - jinja2 dependency removed
    
    Closes #22

--- src/repositories/smtp/repository.py
+++ src/repositories/smtp/repository.py
@@ -4,17 +4,13 @@ import contextlib
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from pathlib import Path
 
-import jinja2
 from email_validator import validate_email, EmailNotValidError
 
 from src.config import settings
 
-_template_path = Path(__file__).parent / "verification-code.jinja2"
-_logo_path = Path(__file__).parent / "logo.png"
-VERIFICATION_CODE_TEMPLATE: jinja2.Template = jinja2.Template(_template_path.read_text(), autoescape=True)
+VERIFICATION_CODE_TEMPLATE = (Path(__file__).parent / "templates/verification-code.html").read_text()
 
 
 class SMTPRepository:
@@ -33,14 +29,9 @@ class SMTPRepository:
 
     def render_verification_message(self, target_email: str, code: str) -> str:
         mail = MIMEMultipart("related")
-        html = VERIFICATION_CODE_TEMPLATE.render(code=code)
+        html = VERIFICATION_CODE_TEMPLATE.replace("${{code}}", code)
         msg_html = MIMEText(html, "html")
         mail.attach(msg_html)
-
-        with open(_logo_path, "rb") as f:
-            logo = MIMEImage(f.read(), name="logo.png")
-            logo.add_header("Content-ID", "logo")
-            mail.attach(logo)
 
         mail["Subject"] = "Music Room: Verification code"
         mail["From"] = settings.SMTP_USERNAME
