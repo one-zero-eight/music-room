6ce74e7848dbe10565f10b4ad89b4d3e1e362558
Ruslan Belkov <ruslan.belckov@yandex.ru>
2024-03-24 22:09:16 +0300

    fix: fix imports

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,7 +1,7 @@
-from src.api.auth import router as router_auth
-from src.api.bookings import router as router_booking
-from src.api.users import router as router_users
-from src.api.root import router as router_root
+from src.api.auth.routes import router as router_auth
+from src.api.bookings.routes import router as router_booking
+from src.api.users.routes import router as router_users
+from src.api.root.routes import router as router_root
 
 routers = [router_root, router_users, router_booking, router_auth]
 
