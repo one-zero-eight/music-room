caf25653cb740d793b88a7191a85022ec8eba431
bebra_dev <fivan999@yandex.ru>
2024-10-09 02:32:16 +0300

    feat(bot): notification message extended with more data

--- src/bot/webserver.py
+++ src/bot/webserver.py
@@ -5,18 +5,27 @@ from pydantic import BaseModel
 from src.dependendies.auth import VerifiedDep
 from src.schemas.auth import VerificationSource
 from src.exceptions import ForbiddenException
+import datetime
 
 
-class UserNotification(BaseModel):
+class BookingInfo(BaseModel):
     telegram_id: int
+    time_start: datetime.datetime
+    time_end: datetime.datetime
 
 
 app = FastAPI(root_path=bot_settings.webhook_url)
 
 
 @app.post("/booking/notify")
-async def notify_user_about_booking(user_data: UserNotification, verification: VerifiedDep) -> Response:
+async def notify_user_about_booking(booking: BookingInfo, verification: VerifiedDep) -> Response:
     if verification.source != VerificationSource.BOT:
         raise ForbiddenException()
-    await bot.send_message(user_data.telegram_id, "Don't forget about your booking! It will start in an hour")
+    seconds_till_booking = (booking.time_start - datetime.datetime.now()).seconds
+    await bot.send_message(
+        booking.telegram_id,
+        f"Don't forget about your booking! It will start in {seconds_till_booking // 60} minutes. "
+        "If you can't attend, please cancel the booking.\n"
+        f"Your booking time: {booking.time_start.strftime("%H:%M")} - {booking.time_end.strftime("%H:%M")}.",
+    )
     return Response(status_code=status.HTTP_200_OK)
