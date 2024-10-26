dddcfee202e704a623619af4b255f3206e26a0a9
bebra_dev <fivan999@yandex.ru>
2024-10-26 23:14:08 +0300

    feat: using api via user jwt token

--- src/repositories/auth/repository.py
+++ src/repositories/auth/repository.py
@@ -1,10 +1,13 @@
 __all__ = ["SqlAuthRepository", "auth_repository", "TokenRepository"]
 
+import time
 from typing import Self
 
+from authlib.jose import JoseError, JWTClaims, jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config import api_settings
+from src.repositories.innohassle_accounts import innohassle_accounts
 from src.schemas.auth import VerificationResult, VerificationSource
 from src.storage.sql import AbstractSQLAlchemyStorage
 
@@ -24,6 +27,15 @@ class TokenRepository:
     ALGORITHM = "RS256"
 
     @classmethod
+    def decode_token(cls, token: str) -> JWTClaims:
+        now = time.time()
+        pub_key = innohassle_accounts.get_public_key()
+        payload = jwt.decode(token, pub_key)
+        payload.validate_exp(now, leeway=0)
+        payload.validate_iat(now, leeway=0)
+        return payload
+
+    @classmethod
     def verify_bot_token(cls, auth_token: str) -> VerificationResult:
         if auth_token.endswith(api_settings.bot_token):
             telegram_id = auth_token[: -len(api_settings.bot_token)]
@@ -35,6 +47,20 @@ class TokenRepository:
             return VerificationResult(success=True, telegram_id=telegram_id, source=VerificationSource.BOT)
         else:
             return VerificationResult(success=False)
+
+    @classmethod
+    async def verify_user_token(cls, token: str) -> VerificationResult:
+        try:
+            payload = cls.decode_token(token)
+            innohassle_id: str = payload.get("uid")
+            if innohassle_id is None:
+                return VerificationResult(success=False)
+            user_data = await innohassle_accounts.get_user_by_innohassle_id(innohassle_id)
+            if user_data is None:
+                raise VerificationResult(success=False)
+            return VerificationResult(success=True, telegram_id=user_data.telegram.id)
+        except JoseError:
+            raise VerificationResult(success=False)
 
     @classmethod
     def verify_api_token(cls, auth_token: str) -> VerificationResult:
